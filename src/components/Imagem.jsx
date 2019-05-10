2579e2613af94763c51a01aa467cf9d6b8cc12a1
cleverson <cleverson.s.dias@gmail.com>
2019-05-10 15:59:33 -0300

    Icon

--- src/components/Imagem.jsx
+++ src/components/Imagem.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
+import { FaThumbsUp, FaEye, FaHeart, FaComments } from 'react-icons/fa'
 
 const Imagem = props => {
-	const { largeImageURL, likes, previewURL, tags, views } = props.imagem
+	const {
+		largeImageURL,
+		likes,
+		previewURL,
+		tags,
+		views,
+		favorites,
+		comments
+	} = props.imagem
 	return (
 		<div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
 			<div className="card">
+				<h6 className="card-header">{tags}</h6>
 				<img src={previewURL} alt={tags} className="card-img-top" />
 				<div className="card-body">
-					<p className="card-text">{likes} curtidas</p>
-					<p className="card-text">{views} Visualizações</p>
+					<p className="card-text">
+						<FaHeart /> {favorites} Favoritos
+					</p>
+					<p className="card-text">
+						<FaComments /> {comments} Comentários
+					</p>
+					<p className="card-text">
+						<FaThumbsUp /> {likes} curtidas
+					</p>
+					<p className="card-text">
+						<FaEye /> {views} Visualizações
+					</p>
+				</div>
+				<div class="card-footer text-muted">
 					<a
 						href={largeImageURL}
 						target="_blank"
